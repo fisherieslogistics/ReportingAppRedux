bac531fd4aac98b8b48104e31072095ec579430a
fllfl <rimu@fisherieslogistics.com>
2016-09-03 07:33:45 +1200

    much better nav --- In theory

--- app/providers/NativeLocation.js
+++ app/providers/NativeLocation.js
@@ -1,11 +1,29 @@
 import React, { DeviceEventEmitter } from 'react-native';
-import { RNLocation } from 'NativeModules';
+//import RNLocation, { Location } from 'NativeModules';
+var { RNLocation: Location } = require('NativeModules');
+
+console.log("RN LOCATION", Location);
+
+//const Location = RNLocation.Location;
 
 class NativeLocation {
 
-  constructor(){
+  constructor(props){
+    this.locationStarted = false;
+    this.locationAuthorized = false;
     this.startUpdatingLocation();
-    this.position = null;
+    this.position = {
+      coords: {
+        speed: -1,
+        longitude: 0,
+        latitude: 0,
+        accuracy: 500,
+        heading: -1,
+        altitude: -7000,
+        altitudeAccuracy: -1
+      },
+      timestamp: 0,
+    };
   }
 
   getPosition(){
@@ -13,14 +31,23 @@ class NativeLocation {
   }
 
   startUpdatingLocation(){
-    RNLocation.requestAlwaysAuthorization();
-    RNLocation.setDistanceFilter(5.0);
-    RNLocation.setDesiredAccuracy(2);
-    RNLocation.startUpdatingLocation();
+    Location.requestAlwaysAuthorization();
+    Location.getAuthorizationStatus((authorization) => {
+      this.locationStarted = true;
+      // authorization is a string which is either "authorizedAlways",
+      // "authorizedWhenInUse", "denied", "notDetermined" or "restricted"
+      if(authorization === "authorizedAlways"){
+        this.locationAuthorized = true;
+        Location.setAllowsBackgroundLocationUpdates(true);
+        Location.setDistanceFilter(5.0);
+        Location.setDesiredAccuracy(2);
+        Location.startUpdatingLocation();
+      }
+    });
+
     const subscription = DeviceEventEmitter.addListener(
       'locationUpdated',
       (location) => {
-        console.log(location);
         this.position = location;
         /* Example location returned
         {
@@ -30,7 +57,7 @@ class NativeLocation {
             latitude: 51.50998,
             accuracy: 5,
             heading: -1,
-            altitude: 0,
+            altitude: -80000,
             altitudeAccuracy: -1
           },
           timestamp: 1446007304457.029
