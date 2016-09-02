a7c2e63d680099b3a7be12266c9edf7aa89795fd
fllfl <rimu@fisherieslogistics.com>
2016-09-03 07:34:04 +1200

    containers and weight always valid

--- app/models/ProductModel.js
+++ app/models/ProductModel.js
@@ -4,8 +4,8 @@ const valid = Validator.valid;
 const model = [
   {label: 'Species', id: 'code', valid: valid.targetProduct, defaultValue: "", type:"product",
     editorDisplay: {editor: 'event', type: 'combined', siblings: ['numberOfContainers', 'weight']}},
-  {label: 'Containers', id: 'numberOfContainers', type: 'number', valid: valid.greaterThanZero, defaultValue: "0", optional: true},
-  {label: 'Weight', id: 'weight', valid: valid.greaterThanZero, type: 'number', defaultValue: "0", unit: 'kg'},
+  {label: 'Containers', id: 'numberOfContainers', type: 'number', valid: valid.alwaysValid, defaultValue: "0", optional: true},
+  {label: 'Weight', id: 'weight', valid: valid.alwaysValid, type: 'number', defaultValue: "0", unit: 'kg'},
   {label: 'State', id: 'state', valid: valid.alwaysValid, defaultValue: "gre",
     editorDisplay: {editor: 'event', type: 'combined', siblings: ['containerType', 'grade', 'treatment']}, optional: true },
   {label: 'Container Type', id: 'containerType', valid: valid.alwaysValid, defaultValue: "", type: 'container', optional: true},
