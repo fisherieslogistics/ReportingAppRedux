0db19fe8dc7e6d4fcddda85ded1a7a7049d98e88
fllfl <rimu@fisherieslogistics.com>
2016-09-09 07:31:12 +1200

    nice querys

--- app/api/Queries.js
+++ app/api/Queries.js
@@ -3,141 +3,128 @@ import Helper from '../utils/Helper';
 import version from '../constants/version';
 const helper = new Helper();
 
-const upsertFishingEvent = (fEvent, tripId) => {
-  const catches = fEvent.products.filter((x) => !!x.code)
-                                 .map((c) => {
-                                     let prod = Object.assign({}, c, {weight: parseInt(c.weight || 0),
-                                                                      numberOfContainers: parseInt(c.numberOfContainers | 0)});
-                                     delete prod["objectId"];
-                                     return prod;
-                                  });
-  let custom = {
+function getCustom(fEvent){
+  const tcer = {
     headlineHeight: fEvent.headlineHeight || 0,
-    wingSpread: fEvent.wingSpread || 0
+    wingSpread: fEvent.wingSpread || 0,
   };
-  const otherCatches = {};
-  ['discards', 'protecteds', 'incidents'].forEach( name => {
-    otherCatches[name] = fEvent[name].map(item => {
-      const newItem = Object.assign({}, item);
-      delete newItem.objectId;
-      return newItem;
-    });
-  });
-  const startLoc = helper.locationToGeoJSON(fEvent.locationAtStart);
-  const endLoc = helper.locationToGeoJSON(fEvent.locationAtEnd);
-  return `
-    mutation {
-      upsertFishingEvent(
-        id: "${fEvent.objectId}",
-        trip_id: "${ tripId }",
-        numberOfInTrip: ${ fEvent.id },
-        nonFishProtected: ${ fEvent.nonFishProtected ? true : false },
-        averageSpeed: ${ parseFloat(fEvent.averageSpeed || 0) },
-        bottomDepth: ${ parseInt(fEvent.bottomDepth || 0)},
-        endDate: "${ fEvent.datetimeAtEnd ? fEvent.datetimeAtEnd.toISOString() : fEvent.datetimeAtStart.toISOString()  }",
-        startDate: "${ fEvent.datetimeAtStart.toISOString() }",
-        finished: true,
-        groundropeDepth: ${ parseInt(fEvent.groundropeDepth || 0) },
-        targetSpecies: "${ fEvent.targetSpecies }",
-        committed: ${ !!fEvent.committed },
-        custom: ${ JSON.stringify(JSON.stringify(custom)) },
-        locationStart: ${startLoc},
-        locationEnd: ${endLoc},
-        catches: ${ util.inspect(catches).replace(/\'/g, '"') },
-        discards: ${ util.inspect(otherCatches.discards).replace(/\'/g, '"') },
-        protecteds: ${ util.inspect(otherCatches.protecteds).replace(/\'/g, '"') },
-        incidents: ${ util.inspect(otherCatches.incidents).replace(/\'/g, '"') },
-        version: ${version},
-      ) {
-        _id,
-      }
-    }
-  `
+  const lcer = {
+    numberOfHooks: fEvent.numberOfHooks || 0,
+    hookSpacing: fEvent.hookSpacing || 0,
+  };
+  if('numberOfHooks' in fEvent){
+    return JSON.stringify(lcer);
+  }
+  return JSON.stringify(tcer);
 }
 
-const upsertLCERFishingEvent = (fEvent, tripId) => {
-  const catches = fEvent.products.filter((x) => !!x.code).map((c) => {
-    let prod = Object.assign({}, c, {weight: parseInt(c.weight || 0),
-                                     numberOfContainers: parseInt(c.numberOfContainers | 0)});
+function parseProducts(products){
+  const catches = products.filter((x) => !!x.code).map((c) => {
+    let prod = Object.assign({}, c, {
+      weight: parseInt(c.weight || 0),
+      numberOfContainers: parseInt(c.numberOfContainers || 0),
+      containerType: c.containerType || "",
+      state: c.state || "",
+      treatment: c.treatment || "iced",
+      grade: c.grade || "",
+    });
     delete prod["objectId"];
     return prod;
   });
-  let custom = {
-    numberOfHooks: fEvent.numberOfHooks || 0,
-    hookSpacing: fEvent.hookSpacing || 0
-  };
-  const startLoc = helper.locationToGeoJSON(fEvent.locationAtStart);
-  const endLoc = helper.locationToGeoJSON(fEvent.locationAtEnd);
+  return catches;
+}
+
+function parseUnsoughtCatches(fEvent){
   const otherCatches = {};
   ['discards', 'protecteds', 'incidents'].forEach( name => {
-    otherCatches[name] = fEvent[name].map(item => {
+    const items = fEvent[name].map(item => {
       const newItem = Object.assign({}, item);
       delete newItem.objectId;
       return newItem;
     });
+    otherCatches[name] = items;
   });
-  return `
-    mutation {
-      upsertFishingEvent(
-        id: "${fEvent.objectId}",
-        trip_id: "${ tripId }",
-        numberOfInTrip: ${ fEvent.id },
-        nonFishProtected: ${ fEvent.nonFishProtected ? true : false },
-        endDate: "${ fEvent.datetimeAtEnd ? fEvent.datetimeAtEnd.toISOString() : fEvent.datetimeAtStart.toISOString()  }",
-        startDate: "${ fEvent.datetimeAtStart.toISOString() }",
-        finished: true,
-        targetSpecies: "${ fEvent.targetSpecies }",
-        committed: ${ !!fEvent.committed },
-        bottomDepth: ${ parseInt(fEvent.bottomDepth || 0)},
-        custom: ${ JSON.stringify(JSON.stringify(custom)) },
-        locationStart: ${startLoc},
-        locationEnd: ${endLoc},
-        catches: ${ util.inspect(catches).replace(/\'/g, '"') }
-        discards: ${ util.inspect(otherCatches.discards).replace(/\'/g, '"') },
-        protecteds: ${ util.inspect(otherCatches.protecteds).replace(/\'/g, '"') },
-        incidents: ${ util.inspect(otherCatches.incidents).replace(/\'/g, '"') },
-        version: ${version},
-      ) {
-        _id,
+  return otherCatches;
+}
+
+
+function upsertFishingEvent(fEvent, tripId) {
+  const otherCatches = parseUnsoughtCatches(fEvent);
+  const catches = parseProducts(fEvent.products);
+  const custom = getCustom(fEvent);
+  const startLoc = helper.locationToGeoJSON(fEvent.locationAtStart);
+  const endLoc = helper.locationToGeoJSON(fEvent.locationAtEnd);
+  const fishingEventInput = {
+    id: fEvent.objectId,
+    trip_id: tripId,
+    numberOfInTrip: fEvent.id,
+    nonFishProtected: !!fEvent.nonFishProtected,
+    averageSpeed: parseFloat(fEvent.averageSpeed || 0),
+    bottomDepth: parseInt(fEvent.bottomDepth || 0),
+    endDate: fEvent.datetimeAtEnd ? fEvent.datetimeAtEnd.toISOString() : null,
+    startDate: fEvent.datetimeAtStart.toISOString(),
+    finished: true,
+    groundropeDepth: parseInt(fEvent.groundropeDepth || 0),
+    targetSpecies: fEvent.targetSpecies,
+    committed: !!fEvent.committed,
+    custom: custom,
+    locationStart: startLoc,
+    locationEnd: endLoc,
+    catches: catches,
+    discards: otherCatches.discards,
+    protecteds: otherCatches.protecteds,
+    incidents: otherCatches.incidents,
+    version: version,
+  };
+  return {
+    query: `
+      mutation($data: UpsertFishingEventMutation2Input!) {
+        upsertFishingEvent2(input: $data)
+        {
+          fishingEvent {
+            id
+          }
+        }
       }
+    `,
+    variables: {
+      fishingEvent: fishingEventInput,
+      clientMutationId: "fisData2" + new Date().getTime(),
     }
-  `
+  };
 }
 
 const upsertTrip = (trip) => {
   let _trip = Object.assign({}, trip, {
-    _id: trip.objectId,
-    startDate: trip.sailingTime.toISOString(),
-    endDate: trip.ETA.toISOString(),
+    id: trip.objectId,
+    startDate: trip.startDate.toISOString(),
+    endDate: trip.endDate.toISOString(),
     vessel_id: trip.vesselId,
-    complete: trip.completed,
-    unloadPort: trip.portTo,
-    leavingPort: trip.portFrom,
+    complete: trip.complete,
+    endPort: trip.endPort,
+    startPort: trip.startPort,
   });
   delete _trip.started;
-  delete _trip.lastSubmitted;
-  delete _trip.lastChange;
   delete _trip.objectId;
   delete _trip.vesselId;
   return {
     query: `
-      mutation($data: UpsertTripMutationInput!){
-        upsertTripMutation(input: $data)
+      mutation($data: UpsertTripMutation2Input!){
+        upsertTrip2(input: $data)
           {
             trip{
-              _id
+              id
             }
           }
       }
     `,
     variables: {
       trip: _trip,
-      clientMutationId: "tripData" + new Date().getTime()
+      clientMutationId: "tripData2" + new Date().getTime()
     }
   };
 }
-
-export {upsertTrip, upsertFishingEvent, upsertLCERFishingEvent};
+export {upsertTrip, upsertFishingEvent};
 
 export default {
   getMe: `
