b48424a4b41ccfb759ea65c4b692daf845f51e8f
fllfl <rimu@fisherieslogistics.com>
2016-09-10 10:52:21 +1200

    faster check  better way to then and catch

--- app/api/SyncWorker.js
+++ app/api/SyncWorker.js
@@ -6,6 +6,8 @@ import Queries, {
 
 import Helper from '../utils/Helper';
 import moment from 'moment';
+import ApiActions from '../actions/ApiActions.js';
+const apiActions = new ApiActions();
 const helper = new Helper();
 const TIMEOUT = 6000;
 
@@ -15,7 +17,7 @@ class SyncWorker {
     this.dispatch = dispatch;
     this.api = api;
     this.getState = getState;
-    this.timeToSync = 7000;
+    this.timeToSync = 3000;
     this.requests = [];
     this.startSync();
   }
@@ -26,10 +28,11 @@ class SyncWorker {
 
   sync(){
     const state = this.getState().default;
+    console.log('sync?');
     if(this.requests.length || (!state.auth.loggedIn)){
       return;
     }
-
+    console.log('sync');
     const formType = state.me.formType;
     const fEventIds = Object.keys(state.sync.fishingEvents);
     this.requests = state.fishingEvents.events.filter(fe => (fEventIds.indexOf(fe.objectId) !== -1))
@@ -38,17 +41,25 @@ class SyncWorker {
       this.requests.push(this.mutateTrip(state.trip, state.me.vessel.id));
     }
 
-    state.sync.queues.pastTrips.slice(0, 1).forEach((t) => {
+    console.log(state.sync.queues.pastTrips.length, "any?");
+    state.sync.queues.pastTrips.forEach((t) => {
+      console.log('mutateTrip', t)
       let pastRequests = [];
       t.fishingEvents.forEach(fe => pastRequests.push(this.mutateFishingEvent(fe, t.trip.objectId, t.formType)));
-      return Promise.all(pastRequests).then(this.mutatePastTrip(t.trip));
+      return this.mutatePastTrip(t.trip, t.vesselId).then((res) => {
+        Promise.all(pastRequests).then();
+      })
     });
 
     if(this.requests.length){
       Promise.all(this.requests).then((responses) => {
         this.requests = [];
+        apiActions.checkMe(this.getState().default.auth, this.dispatch);
       });
     }
+
+
+
   }
 
   mutatePastTrip(trip){
@@ -64,7 +75,7 @@ class SyncWorker {
           time: time
         });
       }catch(e) {
-        console.warn(e, e.stack);
+        console.warn(e);
       }
       return {response: res};
     }
@@ -84,7 +95,7 @@ class SyncWorker {
           objectId: res.data.upsertTrip2.trip.id
         });
       }catch(e) {
-        console.warn(e);
+        //console.warn(e);
       }
       return {response: res};
     }
@@ -112,17 +123,20 @@ class SyncWorker {
   }
 
   performMutation(query, variables, success, dispatch){
-    return this.api.mutate(query, variables, this.getState().default.auth)
-      .then(success)
-      .catch((err) => {
-        console.warn("peromed not nice", err);
-        this.dispatch({
-          type: "syncError",
-          time: new moment(),
-          err: err
-        })
+    return new Promise((resolve, reject) => {
+      this.api.mutate(query, variables, this.getState().default.auth)
+        .then((res) => resolve(success(res)))
+        .catch((err) => {
+          console.warn("peromed not nice", err);
+          this.dispatch({
+            type: "syncError",
+            time: new moment(),
+            err: err
+          });
+          resolve(false);
+        });
       });
-  }
+    }
 
 }
 
