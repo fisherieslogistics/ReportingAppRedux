abf50044143ab6ae25685f8397277b331f1cb83c
fllfl <rimu@fisherieslogistics.com>
2016-11-28 12:18:45 +1300

    set api to my local

--- app/reducers/APIReducer.js
+++ app/reducers/APIReducer.js
@@ -1,12 +1,12 @@
 const EndpointLookup = {
   Production: {
-    ApiEndpoint: 'http://fisherylogistics.com:5003/',
+    ApiEndpoint: 'http://api.fisherylogistics.com:5003/',
   },
   Staging: {
     ApiEndpoint: 'http://fisherieslogistics.com:5003/',
   },
   Local:  {
-    ApiEndpoint: 'http://localhost:5003/',
+    ApiEndpoint: 'http://192.168.178.21:5003/',
   },
   'Rimu PC ASTRO: 192.168.20.119': {
     ApiEndpoint: 'http://192.168.20.119:5003/',
@@ -15,8 +15,8 @@ const EndpointLookup = {
 
 export { EndpointLookup }
 
-export default (state = EndpointLookup.Production, action) => {
-  return {  ApiEndpoint: 'http://fisherieslogistics.com:5003/' };
+export default (state = EndpointLookup.Local, action) => {
+  return EndpointLookup.Local;
   switch(action.type){
     case "devMode":
       return Object.assign({}, state, EndpointLookup[action.payload]);
