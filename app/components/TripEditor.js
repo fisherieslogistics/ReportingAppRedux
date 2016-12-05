50c8e7a5ee184facffc0c38790baf84e3160deae
fllfl <rimu@fisherieslogistics.com>
2016-12-06 00:57:41 +1300

    add new trip components

--- app/components/TripEditor.js
+++ app/components/TripEditor.js
@@ -22,6 +22,7 @@ import PortPicker from './PortPicker';
 import UserActions from '../actions/UserActions';
 import PlaceholderMessage from './common/PlaceholderMessage';
 import ViewActions from '../actions/ViewActions';
+import StartTripEditor from './StartTripEditor';
 
 const viewActions = new ViewActions();
 const userActions = new UserActions();
@@ -29,73 +30,8 @@ const helper = new Helper();
 const tripActions = new TripActions();
 const PickerItemIOS = PickerIOS.Item;
 
-
 const PlaceAndTime = ({portType, timeType, port, time, onChangePort, onChangeTime, disabled, choices}) => {
-  time = time || new moment(new Date().getTime());
-  let placeTimeStyle = StyleSheet.create({
-    wrapper:{
-      backgroundColor: colors.pastelGreen,
-      flex: 0.5,
-      alignItems: 'flex-start',
-      paddingLeft: 5
-    },
-  });
-  let dateAttr = TripModel.find((a) => a.id == timeType);
-  let dateProps = {
-    customStyles: {
-      dateIcon: {
-        height: 0,
-        opacity: 0
-      },
-      dateInput:{
-        borderWidth: 0,
-        opacity: 0,
-        flexDirection: 'row',
-        flex: 1,
-      },
-    },
-    disabled: false
-  };
-    let dateStyle = [textStyles.font];
 
-
-  return (
-    <View style={[styles.halfway, styles.placeAndTime]}>
-      <View style={{left: -22,}}>
-        <View>
-           <Text style={{color: colors.blue}}>
-            {timeType === 'startDate'? 'Start Time' : 'Estimated Return Time'}
-          </Text>
-            <Text style={[textStyles.font, {fontSize: 16}]}>{!time || isNaN(time.unix()) ? "  " : time.format("DD MMM HH:mm") }</Text>
-            <Text style={[dateStyle, {color: colors.darkGray, fontSize: 12, top: 2}]}>{ (!time || isNaN(time.unix()) ) ? "Select date" : time.fromNow() }</Text>
-        </View>
-       <View style={{ position: 'absolute', top: 14, borderBottomWidth: 1, borderColor: colors.gray}}>
-        {AttributeEditor({
-          attribute: dateAttr,
-          value: time,
-          onChange: onChangeTime,
-          extraProps: dateProps
-        }, () => { console.warn('This should store that we are editing a field')})}
-        </View>
-      </View>
-      <View style={[{width: 120, left: 13, marginTop: 20, marginBottom: 10, borderBottomWidth: 1, borderColor: colors.gray }]}>
-             <Text style={{color: colors.blue}}>
-            {timeType === 'startDate'? 'Start Port' : 'End Port'}
-          </Text>
-        <PortPicker
-          name={portType + "__picker"}
-          choices={choices}
-          portType={portType}
-          value={port || ""}
-          placeholder={"Select a port"}
-          textStyle={{color: colors.black}}
-          style={{borderBottomWidth: 1, borderColor: colors.midGray }}
-          onChange={(value) => onChangePort(portType, value)}
-          inputId={"TripEditor__" + portType}
-          disabled={false}
-        />
-      </View>
-    </View>);
 }
 
 class TripEditor extends React.Component {
@@ -129,6 +65,28 @@ class TripEditor extends React.Component {
       return choices;
     }
 
+    renderStartTrip(){
+      return (
+        <StartTripEditor
+          trip={this.props.trip.startPort}
+          time={this.props.trip.startDate}
+          onChangePort={this.onChangePort}
+          onChangeTime={this.onChangeTime}
+          choices={this.state.portChoices}
+        />
+      );
+    }
+
+    renderEndTrip() {
+      <EndTripEditor
+        trip={this.props.trip.startPort}
+        time={this.props.trip.startDate}
+        onChangePort={this.onChangePort}
+        onChangeTime={this.onChangeTime}
+        choices={this.state.portChoices}
+      />
+    }
+
     endTrip(){
       if(this.props.tripCanEnd){
         this.props.endTrip();
@@ -149,35 +107,19 @@ class TripEditor extends React.Component {
       this.props.dispatch(tripActions.updateTrip(id, value, this.props.trip.started));
     }
 
+    renderTripEditor(){
+      if(this.props.tripCanEnd){
+        return this.renderEndTrip();
+      }
+      return this.renderStartTrip();
+    }
+
     render() {
       let LANDSCAPE = (this.props.orientation.indexOf("LANDSCAPE") !== -1);
       return (
         <View style={[styles.wrapper]}>
           <View style={[styles.row, styles.bottomRow]}>
-            <View style={[styles.halfway]}>
-              <PlaceAndTime
-                portType={"startPort"}
-                timeType={"startDate"}
-                port={this.props.trip.startPort}
-                time={this.props.trip.startDate}
-                onChangePort={this.onChangePort}
-                onChangeTime={this.onChangeTime}
-                disabled={this.props.trip.started}
-                choices={this.state.portChoices}
-              />
-            </View>
-            <View style={[styles.halfway]}>
-              <PlaceAndTime
-                portType={"endPort"}
-                timeType={"endDate"}
-                port={this.props.trip.endPort}
-                time={this.props.trip.endDate}
-                onChangePort={this.onChangePort}
-                onChangeTime={this.onChangeTime}
-                disabled={false}
-                choices={this.state.portChoices}
-              />
-            </View>
+            { this.renderTripEditor() }
           </View>
           <View style={[styles.row, styles.topRow]}>
             <View style={[styles.halfway]}>
