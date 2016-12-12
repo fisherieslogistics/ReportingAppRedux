ef9096447d8927e5c8deae997fb2f0647d2ea6c3
fllfl <rimu@fisherieslogistics.com>
2016-12-12 21:50:21 +1300

    Fuck this bloody thing

--- app/reducers/FishingEventReducer.js
+++ app/reducers/FishingEventReducer.js
@@ -7,7 +7,7 @@ import  {getFormModelByTypeCode, getFishingEventModelByTypeCode} from '../utils/
 
 const helper = new Helper();
 
-let initialState = {
+const initialState = {
   events: [],
   deletedProducts: {}
 }
@@ -20,18 +20,24 @@ export default (state = initialState, action) => {
         case 'endTrip':
           return initialState;
         case 'startFishingEvent':
+          const deletedProducts = Object.assign({}, state.deletedProducts);
+          deletedProducts[state.events.length + 1] = [];
+          state = changeState(state, { deletedProducts });
           return newFishingEvent(state, action.location, action.formType);
         case 'endFishingEvent':
           return endFishingEvent(state, action.location, action.id, action.formType);
         case 'cancelFishingEvent':
           return changeState(state, {events: [...state.events.slice(0, state.events.length - 1)]});
         case 'setFishingEventValue':
-          let change = {};
+          const change = {};
           change[action.inputId] = action.value;
+          if(action.inputId === 'bottomDepth'){
+            change.groundropeDepth = action.value
+          }
           return changeEvent(action.fishingEventId - 1, state, change, action.formType);
         case 'setLocationValue':
           //always negative whatever they put it for lat
-          let negativeLatChanges = Object.assign({}, action.changes, {lat: (0 - Math.abs(parseFloat(action.changes.lat)))});
+          const negativeLatChanges = Object.assign({}, action.changes, {lat: (0 - Math.abs(parseFloat(action.changes.lat)))});
           return changeEvent(action.id - 1, state, {locationAtStart: negativeLatChanges}, action.formType);
         case 'changeSpecies':
           return ChangeCatch(action, state, "code", action.formType);
@@ -41,17 +47,12 @@ export default (state = initialState, action) => {
           return ChangeCatch(action, state, action.name, action.formType);
         case 'addProduct':
           state = clearDeletedProducts(action.fishingEventId, state);
-          return addNewCatch(action.fishingEventId, state, action.formType);
+          state = addNewCatch(action.fishingEventId, state, action.formType);
+          return state;
         case 'deleteProduct':
           return stashDeletedProduct(state, action);
         case 'undoDeleteProduct':
           return undoDeleteProduct(state, action);
-        case 'changeEventGear':
-          let gearChange = {};
-          gearChange[action.key] = action.value;
-          state = changeEvent(action.fishingEventId - 1, state, gearChange, action.formType);
-
-          return state;
         case 'formSigned':
           action.fishingEvents.forEach((fe) => {
             state = changeEvent(fe.id - 1, state, {signature: action.signature,
@@ -68,100 +69,106 @@ export default (state = initialState, action) => {
     }
 }
 
+const changeEvent = (index, state, changes, formType) => {
+  changes.lastChange = moment();
+  const updatedEvent = Object.assign({}, state.events[index], changes);
+  updatedEvent.eventValid = calculateEventValid(updatedEvent, formType);
+  updatedEvent.productsValid = calculateEventProductsValid(updatedEvent);
+  return replaceFishingEvent(state, updatedEvent);
+}
+
 const undoDeleteProduct = (state, {fishingEventId, formType}) => {
-  let product = state.deletedProducts[fishingEventId].pop();
+  const product = state.deletedProducts[fishingEventId].pop();
   return addProductToEvent(fishingEventId, product, state, formType);
 }
 
 const clearDeletedProducts = (fishingEventId, state) => {
-  let change = {};
+  const change = {};
   change[fishingEventId] = [];
-  let deleted = Object.assign(state.deletedProducts, change);
+  const deleted = Object.assign(state.deletedProducts, change);
   return Object.assign(state, {deletedProducts: deleted});
 }
 
 const stashDeletedProduct = (state, {fishingEventId, productIndex, formType}) => {
-  let fishingEvent = state.events[fishingEventId -1];
-  let product = fishingEvent.products[productIndex];
-  let deletedProducts = Object.assign({}, state.deletedProducts);
+  const fishingEvent = state.events[fishingEventId -1];
+  const product = fishingEvent.products[productIndex];
+  const deletedProducts = Object.assign({}, state.deletedProducts);
   if(fishingEventId in deletedProducts){
     deletedProducts[fishingEventId].push(product);
   }else{
     deletedProducts[fishingEventId] = [product];
   }
-  let newProducts = [
+  const newProducts = [
     ...fishingEvent.products.slice(0, productIndex),
     ...fishingEvent.products.slice(productIndex + 1, fishingEvent.products.length)
   ];
   state = changeEvent(fishingEventId-1, state, {products: newProducts}, formType);
-  return changeState(state, {deletedProducts: deletedProducts});
+  return changeState(state, { deletedProducts });
 }
 
-const changeState = (state, change) => {
-  return Object.assign({}, state, change);
-}
+const changeState = (state, change) => Object.assign({}, state, change)
 
-const changeEvent = (index, state, changes, formType) => {
-  changes.lastChange = moment();
-  updatedEvent = Object.assign({}, state.events[index], changes);
-  updatedEvent.eventValid = calculateEventValid(updatedEvent, formType);
-  updatedEvent.groundropeDepth = updatedEvent.bottomDepth;
-  return replaceFishingEvent(state, updatedEvent);
-}
-
-const replaceFishingEvent = (state, updatedEvent) => {
-  return Object.assign({}, state,
+const replaceFishingEvent = (state, updatedEvent) => Object.assign({}, state,
     {events: [
       ...state.events.slice(0, updatedEvent.id -1),
       updatedEvent,
       ...state.events.slice(updatedEvent.id)
     ]
   });
-}
 
-const blankCatch = () => {
-  return ModelUtils.blankModel(ProductModel);
-}
-
-const getNewProduct = (fishingEventId, state) => {
-  let product = blankCatch();
-  let fishingEvent = state.events[fishingEventId-1];
-  if(fishingEventId > 1){
-    const previousEvent = state.events[fishingEventId - 2];
-    const previousProduct = previousEvent.products[fishingEvent.products.length];
-    if(previousProduct){
-      ['code', 'containerType', 'treatment', 'state'].forEach((a) => {
-        product[a] = previousProduct[a] || product[a];
-      });
+const calculateEventProductsValid = (fEvent) => {
+  let valid = true;
+  const usedCodes = [];
+  fEvent.products.forEach(p => {
+    if(!p.code || !p.weight) {
+      valid = false;
     }
-  }
-  return product;
+    if(usedCodes.indexOf(p.code) !== -1){
+      valid = false;
+    }
+    usedCodes.push(p.code);
+  });
+  return valid && fEvent.products.length;
 }
 
-const addNewCatch = (fishingEventId, state, formType) => {
-  let product = getNewProduct(fishingEventId, state);
-  return addProductToEvent(fishingEventId, product, state, formType);
+const calculateEventValid = (fEvent, formType) => {
+  const fishingEventModel = getFishingEventModelByTypeCode(formType);
+  let valid = true;
+  fishingEventModel.complete.forEach((attr) => {
+    if(attr.valid){
+      if(attr.valid && !attr.valid.func(fEvent[attr.id])){
+        valid = false;
+      }
+    }
+  });
+  return valid;
 }
 
 const addProductToEvent = (fishingEventId, product, state, formType) => {
-  let fishingEventProducts = [...state.events[fishingEventId -1].products, product];
+  const fishingEventProducts = [...state.events[fishingEventId -1].products, product];
   return changeEvent(fishingEventId -1, state, {products: fishingEventProducts}, formType);
 }
 
+const addNewCatch = (fishingEventId, state, formType) => {
+  const product = ModelUtils.blankModel(ProductModel);
+  return addProductToEvent(fishingEventId, product, state, formType);
+}
+
 const ChangeCatch = (action, state, attr, formType) => {
-  let fishingEventIndex = action.fishingEventId - 1;
-  let change = {}
+  const fishingEventIndex = action.fishingEventId - 1;
+  const change = {}
   change[attr] = action.value;
-  let newCatch = Object.assign({}, state.events[fishingEventIndex].products[action.catchIndex], change)
-  let fishingEventChange = {};
+  const newCatch = Object.assign({}, state.events[fishingEventIndex].products[action.catchIndex], change)
+  const fishingEventChange = {};
   fishingEventChange.products = [
       ...state.events[fishingEventIndex].products.slice(0, action.catchIndex),
       newCatch,
       ...state.events[fishingEventIndex].products.slice(action.catchIndex + 1)
   ];
-  updatedEvent = Object.assign({}, state.events[fishingEventIndex], fishingEventChange);
+  const updatedEvent = Object.assign({}, state.events[fishingEventIndex], fishingEventChange);
   updatedEvent.lastChange = moment();
   updatedEvent.eventValid = calculateEventValid(updatedEvent, formType);
+  updatedEvent.productsValid = calculateEventProductsValid(updatedEvent);
   return Object.assign({}, state, {events: [
       ...state.events.slice(0, fishingEventIndex),
       updatedEvent,
@@ -170,11 +177,11 @@ const ChangeCatch = (action, state, attr, formType) => {
 }
 
 const endFishingEvent = (state, location, id, formType) => {
-  let change = {};
+  const change = {};
   change.datetimeAtEnd = moment();
   change.locationAtEnd = location;
   change.nonFishProtected = false;
-  let fishingEventToUpdate = Object.assign({}, state.events[id - 1], change);
+  const fishingEventToUpdate = Object.assign({}, state.events[id - 1], change);
   fishingEventToUpdate.eventValid = calculateEventValid(fishingEventToUpdate, formType);
   return Object.assign({}, state, {events: [
       ...state.events.slice(0, id - 1),
@@ -183,52 +190,31 @@ const endFishingEvent = (state, location, id, formType) => {
   ]});
 };
 
-const calculateEventValid = (fEvent, formType) => {
-  const fishingEventModel = getFishingEventModelByTypeCode(formType);
-  let valid = true;
-  let productsValid = !fEvent.products.find(p => !(p.weight && p.code)) && fEvent.products.length;
-  fishingEventModel.complete.forEach((attr) => {
-    if(attr.valid){
-      if(attr.valid && !attr.valid.func(fEvent[attr.id])){
-        valid = false;
-      }
-    }
-  });
-  return valid && productsValid;
-};
-
-const setFishingEventGear = (fishingEvent, gear) => {
-  Object.keys(gear).forEach((k) => {
-    if(allFishingEventAttrs.indexOf(k) !== -1){
-      fishingEvent[k] = gear[k];
-    }
-  });
-  return fishingEvent;
-}
-
 const newFishingEvent = (state, location, formType) => {
   const fishingEventModel = getFishingEventModelByTypeCode(formType);
-  let newEvent = ModelUtils.blankModel(fishingEventModel.complete, 'FishingEvent');
-  let id = state.events.length + 1;
-  const objectId = newEvent.objectId;
-  newEvent.id = id;
-  let deletedProducts = Object.assign({}, state.deletedProducts);
-  deletedProducts[id] = [];
-  state = changeState(state, {deletedProducts: deletedProducts});
+  const newEvent = ModelUtils.blankModel(fishingEventModel.complete, 'FishingEvent');
+  newEvent.id = state.events.length + 1;
   newEvent.datetimeAtStart = moment();
-  let Location = Object.assign({}, location);
-  newEvent.locationAtStart = Location;
+  newEvent.locationAtStart = Object.assign({}, location);
   newEvent.products = [];
-  let previousEvent = state.events.length ? Object.assign({}, state.events[state.events.length - 1]) : null;
-  if(previousEvent){
-    newEvent.targetSpecies = "" + previousEvent.targetSpecies;
-    fishingEventModel.specific.filter(attr => attr.repeating).forEach((attribute) => {
-      let update = {};
-      update[attribute.id] = previousEvent[attribute.id];
-      newEvent = Object.assign({}, newEvent, update);
-    });
+  for(let i = 0; i < 8; i++) {
+    newEvent.products.push(ModelUtils.blankModel(ProductModel));
   }
-  newEvent.objectId = objectId;
+  newEvent.products[7].code = 'OTH';
+  newEvent.products[7].weight = 0;
+  const previousEvent = state.events[newEvent.id - 2];
+  if(previousEvent){
+    fishingEventModel.complete.filter(attr => attr.repeating).forEach((attribute) => {
+      newEvent[attribute.id] = previousEvent[attribute.id];
+    });
+    for(let i = 0; i < previousEvent.products.length; i++) {
+      if(previousEvent.products[i].code !== 'OTH'){
+        newEvent.products[i].code = previousEvent.products[i].code;
+      }
+    }
+  }
+  newEvent.eventValid = calculateEventValid(newEvent, formType);
+  newEvent.productsValid = calculateEventProductsValid(newEvent);
   return Object.assign({}, state, {
       events: [
           ...state.events,
