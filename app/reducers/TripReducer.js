f9de36bd1b360c8c1983cf0398efb4b8ae85b2ad
fllfl <rimu@fisherieslogistics.com>
2016-12-12 21:49:24 +1300

    better for start trip

--- app/reducers/TripReducer.js
+++ app/reducers/TripReducer.js
@@ -3,23 +3,27 @@ import moment from 'moment';
 import ModelUtils from '../utils/ModelUtils';
 import TripModel from '../models/TripModel';
 
-const initialState = ModelUtils.blankModel(TripModel, 'Trip');
-const startDate = new moment();
-initialState.startDate = startDate;
-initialState.endDate = startDate.add(2, "days");
+function newTrip(){
+  const trip = ModelUtils.blankModel(TripModel, 'Trip');
+  const startDate = new moment();
+  trip.startDate = startDate;
+  trip.endDate = startDate.clone().add(2, "days");
+  return trip;
+}
+
+const initialState = newTrip();
 
 const TripReducer = (state = initialState, action) => {
 
     switch(action.type) {
         case 'endTrip':
-            state.complete = true;
-            return state;
+            return newTrip();
         case 'updateTrip':
             return Object.assign({}, state, action.update);
         case 'startTrip':
             return Object.assign({}, state, { started: true, vesselId: action.vesselId });
     }
-    
+
     return Object.assign({}, state);
 };
 
