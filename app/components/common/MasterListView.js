d684c0ac5e216a2af117914e5df4543229d2f759
fllfl <rimu@fisherieslogistics.com>
2016-12-12 21:44:29 +1300

    change look of list - cleanup

--- app/components/common/MasterListView.js
+++ app/components/common/MasterListView.js
@@ -10,47 +10,57 @@ import React from 'react';
 
 import {listViewStyles, colors} from '../../styles/styles';
 
-const renderSeperator = (sectionID: number, rowID: number, adjacentRowHighlighted: bool) => {
-  return (
+const renderSeperator = (sectionID: number, rowID: number, adjacentRowHighlighted: bool) => (
     <View
       key={`${sectionID}-${rowID}`}
       style={listViewStyles.seperator}
     />
-  );
-}
+  )
 
 const renderRow = (item, sectionId, rowId, props) => {
-  let selected = props.isSelected(item, rowId);
-  let rowStyle = selected ? listViewStyles.selectedListRow : {}
+  const selected = props.isSelected(item, rowId);
+  const rowStyle = selected ? listViewStyles.selectedListRow : {}
+  let icon = null;
+  if(props.getIcon) {
+    icon = (
+      <View style={[listViewStyles.listRowItemTiny]}>
+        {props.getIcon(item, selected)}
+      </View>
+    );
+  }
   return (
     <TouchableHighlight
       onPress={() => {
         props.onPress(item, rowId);
       }}
-      underlayColor={colors.blue}
+      underlayColor={ colors.blue }
       activeOpacity={0.3}
     >
       <View style={[listViewStyles.listRow, rowStyle]}>
-        <View style={[listViewStyles.listRowItemTiny]}>
-          {props.getIcon(item, selected)}
-        </View>
+        { icon }
         {props.getDescription(item, sectionId, rowId, selected)}
       </View>
     </TouchableHighlight>);
 }
 
+const renderScroll = (props) => (<RecyclerViewBackedScrollView {...props} />);
+
 const MasterListView = (props) => {
+  let renderAddProps = (item, sectionId, rowId) => renderRow(item, sectionId, rowId, props);
+  if(props.renderRow) {
+    renderAddProps = (item, sectionId, rowId) => props.renderRow(item, sectionId, rowId, props);
+  }
   return (
     <ListView
       style={[listViewStyles.listView]}
-      enableEmptySections={true}
+      enableEmptySections
       dataSource={props.dataSource}
-      renderRow={(item, sectionId, rowId) => renderRow(item, sectionId, rowId, props)}
-      renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+      renderRow={renderAddProps}
+      renderScrollComponent={renderScroll}
       renderSeparator={renderSeperator}
     />
   );
-};
+}
 
 export {renderSeperator, renderRow}
 export default MasterListView
