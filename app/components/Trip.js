4d093d1b006f5da6b12c20e22eb2c4e2041da863
fllfl <rimu@fisherieslogistics.com>
2016-12-12 21:51:42 +1300

    new Trip - or home component - needs broken up

--- app/components/Trip.js
+++ app/components/Trip.js
@@ -2,62 +2,167 @@
 import {
   View,
   ListView,
+  TouchableOpacity,
   Text,
-  AlertIOS,
-  Image
+  StyleSheet,
 } from 'react-native';
 
-import React, { Component } from 'react';
+import React from 'react';
 import MasterDetailView from './layout/MasterDetailView';
 import TripActions from '../actions/TripActions';
-import { colors } from '../styles/styles';
+
+import { darkColors as colors, listViewStyles, iconStyles, textStyles} from '../styles/styles';
+import Icon8 from '../components/common/Icon8';
+
 import {connect} from 'react-redux';
 import Helper from '../utils/Helper';
 import { MasterToolbar, DetailToolbar } from './layout/Toolbar';
-import TripEditor from './TripEditor';
+import StartTripEditor from './StartTripEditor';
 import TotalsList from './TotalsList';
-import textStyles from '../styles/text';
-import PlaceholderMessage from './common/PlaceholderMessage';
+import MasterListView from './common/MasterListView';
 import moment from 'moment';
+
 
 //let user add a new port if the port is not there
 //let the user know why they cant start or end trip
 
 const helper = new Helper();
 const tripActions = new TripActions();
+const masterListChoices = [
+  'Trip',
+  'Totals',
+];
+const iconNames = {
+  'Totals': 'fishing',
+  'Trip': 'fishing-boat-filled',
+}
+const myListViewStyles = StyleSheet.create(listViewStyles);
 
 class Trip extends React.Component {
   constructor (props){
     super(props);
     this.state = {
-      ds: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id}),
+      dsTotals: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id}),
+      dsPage: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 }),
+      dsTrips: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 }),
+      pastTrips: {},
+      selectedDetail: 'Trip',
+      totals: [],
     };
-    this.isSelected = this.isSelected.bind(this);
+    if(!props.trip.startDate){
+      this.updateTrip("startDate", new moment());
+    }
     this.startTrip = this.startTrip.bind(this);
-  }
-
-  componentWillMount(){
+    this.endTrip = this.endTrip.bind(this);
+    this.renderMasterListView = this.renderMasterListView.bind(this);
+    this.getMasterDescription = this.getMasterDescription.bind(this);
+    this.isDetailSelected = this.isDetailSelected.bind(this);
+    this.masterListOnPress = this.masterListOnPress.bind(this);
+    this.renderMasterIcon = this.renderMasterIcon.bind(this);
+    this.renderTotalsListView = this.renderTotalsListView.bind(this);
+    this.renderDetailView = this.renderDetailView.bind(this);
+    this.renderTripsList = this.renderTripsList.bind(this);
+    this.tripsListOnPress = this.tripsListOnPress.bind(this);
+    this.isTripSelected = this.isTripSelected.bind(this);
+    this.getTripDescription = this.getTripDescription.bind(this);
   }
 
   updateTrip(attribute, value){
     this.props.dispatch(tripActions.updateTrip(attribute, value, this.props.trip.started));
   }
 
-  totalSelected(total, rowId){
-
+  tripsListOnPress(trip){
+    const change = {};
+    const id = trip.objectId;
+    change[id] = this.state.pastTrips[id] ? false : trip;
+    const trips = Object.assign({}, this.state.pastTrips, change);
+    let products = [];
+    Object.keys(trips).forEach((k) => {
+      const pt = trips[k];
+      if(pt){
+        pt.fishingEvents.forEach(fe => {
+          products = products.concat(fe.products);
+        });
+      }
+    });
+    const totals = helper.getTotals(products);
+    this.setState({
+      totals: [{ code: 'Species Code', weight: 'Total KG' }, ...totals],
+      pastTrips: trips,
+    });
   }
 
-  isSelected(){
-
-  }
-
-  renderListView(){
+  renderTotalsListView(){
+    const wrapStyles = {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: 30,
+    }
+    const data = this.state.dsTotals.cloneWithRows(this.state.totals);
     return (
-      <TotalsList
-        data={this.state.ds.cloneWithRows(this.props.totals)}
-        selectedTotal={this.props.selectedTotal}
-        onPress={this.totalSelected}
-        isSelected={this.isSelected}
+      <View style={ wrapStyles }>
+        <TotalsList
+          data={ data }
+          selectedTotal={ this.props.selectedTotal}
+          onPress={ this.totalSelected }
+          isSelected={ this.isSelected }
+        />
+      </View>
+    );
+  }
+
+  getMasterDescription(choice) {
+    const textColor = this.isDetailSelected(choice) ? colors.white : colors.black;
+    const myStyles = [
+      textStyles.font,
+      { color: textColor, fontSize: 18 },
+    ];
+    const viewStyles = { marginLeft: 2, alignItems: 'flex-start', paddingTop: 5};
+    return (
+      <View
+        style={ [myListViewStyles.listRowItem, viewStyles] }
+        key={`${choice}___Trip_Page` }>
+        <Text style={ myStyles }>
+          { choice }
+        </Text>
+      </View>
+    );
+  }
+
+  renderMasterIcon(detailName){
+    const isSelected = this.isDetailSelected(detailName);
+    let backgroundStyle = { backgroundColor: colors.blue, color: colors.white };
+    if(isSelected){
+      backgroundStyle = { backgroundColor: colors.white, color: colors.blue };
+    }
+    return (
+      <Icon8
+        name={iconNames[detailName]}
+        size={30}
+        color="white"
+        style={[iconStyles, backgroundStyle]}
+      />
+    );
+  }
+
+  masterListOnPress(choice) {
+    this.setState({
+      selectedDetail: choice,
+    });
+  }
+
+  isDetailSelected(choice) {
+    return choice === this.state.selectedDetail
+  }
+
+  renderMasterListView() {
+    return (
+      <MasterListView
+        getDescription={ this.getMasterDescription }
+        isSelected={ this.isDetailSelected }
+        onPress={ this.masterListOnPress }
+        dataSource={ this.state.dsPage.cloneWithRows(masterListChoices) }
+        getIcon={ this.renderMasterIcon }
       />
     );
   }
@@ -67,48 +172,174 @@ class Trip extends React.Component {
     this.props.startTripCallback();
   }
 
+  endTrip(){
+    this.props.dispatch(tripActions.endTrip(
+      this.props.trip,
+      this.props.fishingEvents,
+      this.props.vesselId,
+      "",
+    ));
+  }
+
   renderMessage(){
     let message = "";
     if(this.props.trip.started){
-      message = this.props.tripCanEnd ? "Trip started - OK To End Trip" : "Complete all shots and sign all forms before ending trip";
+      message = this.props.tripCanEnd ? "OK To End Trip" : "Complete all shots and sign all forms before ending trip";
     }else {
-      message = this.props.tripCanStart ? "Ready to start trip" :  "Select ports and times before starting trip";
+      message = this.props.tripCanStart ? "Ready to start trip" :  "Select ports and ETA before starting trip";
     }
+    const style = {
+      flex: 1,
+      flexDirection: 'column',
+      alignSelf: 'stretch',
+      alignItems: 'center',
+      padding: 4,
+    };
+    const textStyle = {
+      color: colors.green,
+      marginTop: 0,
+      fontSize: 20,
+    };
     return (
-      <PlaceholderMessage text={message} height={this.props.height } />
+      <View style={[style]}>
+        <Text style={textStyle}>
+          {message}
+        </Text>
+      </View>
     );
   }
 
-  renderMasterView(){
+  getTripDescription(trip, sectionId, rowId) {
+    const isSelected = this.isTripSelected(trip);
+    const textColor = isSelected ? colors.white : colors.black;
+    const myStyles = [
+      textStyles.font,
+      { color: textColor, fontSize: 18 },
+    ];
+    const viewStyles = { alignItems: 'flex-start', paddingTop: 0};
+    const date = trip.startDate || new moment();
+    const parts = [
+      `${trip.startPort}`,
+      date.format('DD MM YYYY'),
+    ];
+    const start = date.unix();
+    return parts.map((p, i) => (
+        <View
+          style={ [myListViewStyles.listRowItem, viewStyles] }
+          key={ `${start}___${rowId}Trip_${sectionId}_list_${i}` }>
+          <Text style={ myStyles }>
+            { p }
+          </Text>
+        </View>
+      ));
+  }
+
+  isTripSelected(trip) {
+    return !!this.state.pastTrips[trip.objectId];
+  }
+
+  renderTripsList(){
+    const trips = [...this.props.history.pastTrips];
+    const currentTrip = helper.getHistoryTrip(
+      Object.assign(this.props.trip, { fishingEvents: this.props.fishingEvents }));
+    const allTrips = [currentTrip, ...trips.reverse()].filter(pt => !!pt.fishingEvents);
     return (
-      <View style={[{padding: 0, flexDirection: 'column', flex: 1 }]}>
-        <View style={[{flexDirection: 'row', flex: 1}]}>
-          { this.renderMessage() }
+      <MasterListView
+        getDescription={ this.getTripDescription }
+        isSelected={ this.isTripSelected }
+        onPress={ this.tripsListOnPress }
+        dataSource={ this.state.dsPage.cloneWithRows(allTrips) }
+        getIcon={ null }
+      />
+    );
+  }
+
+  renderLowerList(){
+    switch (this.state.selectedDetail) {
+      case 'Trip':
+        return this.renderMessage();
+      case 'Totals':
+        return this.renderTripsList();
+      default:
+        break;
+    }
+  }
+
+  renderMasterView(){
+    const outerStyle = {padding: 0, flexDirection: 'column', flex: 1 };
+    const innerStyle = { flexDirection: 'row', flex: 0.3 };
+    const innerStyleBelow = { flexDirection: 'row', flex: 0.7 };
+
+    const masterListView = this.renderMasterListView();
+    const lowerList = this.renderLowerList();
+    return (
+      <View style={[outerStyle]}>
+        <View style={[innerStyle]}>
+          { masterListView }
+        </View>
+        <View style={[innerStyleBelow]}>
+          { lowerList }
         </View>
       </View>
     );
   }
 
-  renderDetailView(){
-    return (
-      <TripEditor
-        trip={this.props.trip}
-        dispatch={this.props.dispatch}
-        tripCanStart={this.props.tripCanStart}
-        tripCanEnd={this.props.tripCanEnd}
-        endTrip={this.endTrip}
-        ports={this.props.ports}
-        startTrip={this.startTrip}
-        orientation={this.props.orientation}
-        onChange={this.updateTrip}
+  getMasterToolbar() {
+    let onPress = () => {};
+    let backgroundColor = colors.darkGray;
+    let text = "Start Trip";
+    let textColor = colors.gray;
+
+    if(this.props.tripCanStart) {
+      onPress = this.startTrip
+      backgroundColor = colors.green;
+      textColor = colors.white;
+    }
+    if(this.props.tripCanEnd) {
+      onPress = this.endTrip
+      backgroundColor = colors.red;
+      textColor = colors.white;
+      text = "End Trip";
+    }
+
+    const buttonStyle = { flex: 1, flexDirection: 'column', alignItems: 'center', backgroundColor, alignSelf: 'stretch'};
+    const innerViewStyle = {alignItems: 'center'}
+    const textStyle = { fontSize: 30, fontWeight: '500', color: textColor, textAlign: 'center', marginTop: 20 };
+    const eventButton = (
+      <TouchableOpacity onPress={onPress} style={[buttonStyle]}>
+         <View style={ innerViewStyle }>
+          <Text style={ textStyle }>
+            { text }
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+    return(
+      <MasterToolbar
+        center={eventButton}
       />
     );
   }
 
+  renderDetailView(){
+    switch (this.state.selectedDetail) {
+      case 'Trip':
+        return (
+          <StartTripEditor
+            trip={this.props.trip}
+            dispatch={this.props.dispatch}
+          />
+        );
+      case 'Totals':
+        return this.renderTotalsListView()
+      default:
+    }
+  }
+
   render(){
-    let toolbarStyle = {height: 20, flex: 0, backgroundColor: colors.backgrounds.dark};
+    const toolbarStyle = {flex: 1,  alignSelf: 'stretch', backgroundColor: colors.black };
     const detailToolbar = (<DetailToolbar style={toolbarStyle} />);
-    const masterToolbar = (<MasterToolbar style={toolbarStyle} />);
+    const masterToolbar = this.getMasterToolbar();
     return (
       <MasterDetailView
         master={ this.renderMasterView() }
@@ -118,26 +349,22 @@ class Trip extends React.Component {
       />
     );
   }
-};
+}
 
-const select = (State, dispatch) => {
-  let state = State.default;
-  let allProducts = state.fishingEvents.events.map(fe => fe.products);
-  let totals = helper.getTotals([].concat.apply(allProducts));
+const select = (State) => {
+  const state = State.default;
   return {
     fishingEvents: state.fishingEvents.events,
     height: state.view.height,
     user: state.me.user,
     trip: state.trip,
-    totals: Object.keys(totals).map((t) => {
-      return {code: t, weight: totals[t]};
-    }),
     ports: state.me.ports,
     tripStarted: state.trip.started,
     tripCanStart: helper.tripCanStart(state.trip),
     tripCanEnd: state.trip.started && (state.fishingEvents.events.find(f => !f.signature) === undefined),
     orientation: state.view.orientation,
-    vesselId: state.me.vessel.id
+    vesselId: state.me.vessel.id,
+    history: state.history,
   };
 }
 
