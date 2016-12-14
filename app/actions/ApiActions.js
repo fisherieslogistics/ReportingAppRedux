673af6cd0cfdbf7b549b0001913d26b8a088aa74
fllfl <rimu@fisherieslogistics.com>
2016-12-14 13:22:01 +1300

    cleanup

--- app/actions/ApiActions.js
+++ app/actions/ApiActions.js
@@ -1,86 +1,24 @@
-"use strict";
 
 import Client from '../api/Client';
 import queries from '../api/Queries';
 import AuthActions from './AuthActions';
 import UserActions from './UserActions';
 import Helper from '../utils/Helper';
-import {
-  AlertIOS,
-} from 'react-native';
 
 const userActions = new UserActions();
 const authActions = new AuthActions();
 const helper = new Helper();
 let client;
-class ApiActions {
-
-  setUpClient(dispatch, ApiEndpoint, AuthEndpoint){
-    client = new Client(dispatch, ApiEndpoint, AuthEndpoint);
-  }
-
-  checkMe(auth, dispatch){
-    client.query(queries.getMe, auth)
-      .catch((err) => {
-        console.warn(err);
-      })
-      .then((res) => {
-        dispatch(userActions.setUser(parseUser(res.data.viewer)));
-      });
-  }
-
-  login(username, password){
-    return (dispatch, getState) => {
-      client.login(username, password)
-        .catch((err) => {
-          return dispatch(authActions.loginError("please try that again "));
-        })
-        .then((auth) => {
-          if(!auth){
-            return dispatch(authActions.loginError("please try that again " + auth));
-          }else{
-            //TODO: This needs to be in a popup dispatcher and not in the logic here
-            AlertIOS.alert(
-              "Login Succes!",
-              'Press Ok to continue',
-              [
-                {text: 'Ok', onPress: () => {
-                  return;
-                }, style: 'cancel'},
-              ]
-            );
-
-          }
-          dispatch(authActions.setAuth(auth));
-          client.query(queries.getMe, helper.updateAuth({}, auth))
-            .catch((err) => {
-              console.warn(err);
-            })
-            .then((res) => {
-              let viewer = res.data.viewer;
-              dispatch(userActions.setVessels(viewer.vessels));
-              if(viewer.vessels.length){
-                dispatch(userActions.setVessel(viewer.vessels[0]));
-              }
-              dispatch(userActions.setUser(parseUser(viewer)));
-            });
-
-        });
-     }
-  }
-
-  query(query, auth, callback){
-
-  }
-
-  mutate(mutation, variables, auth){
-    return client.mutate(mutation, variables, auth);
-  }
-
-}
 
 const parseUser = (viewer) => {
-  const dummy = {};
+  const dummy = {
+    firstName: 'First name',
+    lastName: 'Last Name',
+    permitHolderName: 'Permit Holder Name',
+    permitHolderNumber: 'Permit Holder Number',
+    email: 'test@test.com',
+    bins: [],
+  };
   return viewer ? {
     firstName: viewer.firstName,
     lastName: viewer.lastName,
@@ -89,6 +27,48 @@ const parseUser = (viewer) => {
     email: viewer.email,
     bins: viewer.bins,
   } : dummy;
+}
+
+class ApiActions {
+
+  setUpClient(dispatch, ApiEndpoint, AuthEndpoint){
+    client = new Client(dispatch, ApiEndpoint, AuthEndpoint);
+  }
+
+  checkMe(auth, dispatch){
+    client.query(queries.getMe, auth).then((res) => {
+      if(res && res.data){
+        dispatch(userActions.setUser(parseUser(res.data.viewer)));
+      }
+    });
+  }
+
+  login(username, password){
+    return (dispatch) => {
+      client.login(username, password).then((auth) => {
+        if(!auth){
+          return dispatch(authActions.loginError("please try that again "));
+        }
+        dispatch(authActions.setAuth(auth));
+        client.query(queries.getMe, helper.updateAuth({}, auth))
+          .then((res) => {
+            if(res && res.data){
+              const viewer = res.data.viewer;
+              dispatch(userActions.setVessels(viewer.vessels));
+              if(viewer.vessels.length){
+                dispatch(userActions.setVessel(viewer.vessels[0]));
+              }
+              dispatch(userActions.setUser(parseUser(viewer)));
+            }
+          });
+      });
+    }
+  }
+
+  mutate(mutation, variables, auth){
+    return client.mutate(mutation, variables, auth);
+  }
+
 }
 
 export default ApiActions;
