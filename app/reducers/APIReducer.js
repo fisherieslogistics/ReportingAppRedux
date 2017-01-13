241e80b1c964e939391564cf852976e811c1cdd9
Shavaun MacArthur <shavaunmac@gmail.com>
2017-01-13 13:39:32 +1300

    Got it working with local and fixed icons

--- app/reducers/APIReducer.js
+++ app/reducers/APIReducer.js
@@ -4,6 +4,7 @@ const EndpointLookup = {
   Local: '192.168.178.41',
   'Rimu PC ASTRO': '192.168.20.119',
   JarethIp: '192.168.20.135',
+  ShavaunIP: '192.168.20.110',
 }
 const port = 5003;
 
@@ -15,14 +16,14 @@ function getEndpointObject(key){
   };
 }
 
-const production = getEndpointObject('Production');
+const local = getEndpointObject('ShavaunIP');
 
-export default (state = production, action) => {
+export default (state = local, action) => {
   switch(action.type){
     case "devMode":
       return Object.assign({}, state, getEndpointObject(action.payload));
     default:
-      return production;
+      return local;
   }
 }
 
