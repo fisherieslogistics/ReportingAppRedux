3fa37ccb576a397700bbd6d700a357c938e8eda7
fllfl <rimu@fisherieslogistics.com>
2017-01-21 06:57:58 +1300

    mak eth eprofile editor edit the profile

--- app/components/ProfileEditor.js
+++ app/components/ProfileEditor.js
@@ -1,43 +1,76 @@
 
 import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
 import UserModel from '../models/UserModel';
 import ModelEditor from './common/ModelEditor';
 import VesselModel from '../models/VesselModel';
+import UserActions from '../actions/UserActions';
 
-const ProfileModel = UserModel.concat(VesselModel);
+const userActions = new UserActions();
 
 class ProfileEditor extends Component {
 
     constructor(props) {
       super(props);
-       this.getEditorProps = this.getEditorProps.bind(this);
+      this.getEditorProps = this.getEditorProps.bind(this);
+      this.getUserEditorProps = this.getUserEditorProps.bind(this);
+      this.getVesselEditorProps = this.getVesselEditorProps.bind(this);
+      this.onChangeUser = this.onChangeUser.bind(this);
+      this.onChangeVessel = this.onChangeVessel.bind(this);
     }
 
-    onChange() {
-
+    onChangeUser(inputId, value) {
+      this.props.dispatch(userActions.updateUser(inputId, value));
     }
 
-    getEditorProps(attribute){
-      const profile = Object.assign({}, this.props.user, this.props.vessel);
+    onChangeVessel(inputId, value) {
+      this.props.dispatch(userActions.updateVessel(inputId, value));
+    }
+
+    getUserEditorProps(attribute){
+     return this.getEditorProps(attribute, this.props.user);
+    }
+
+    getEditorProps(attribute, values) {
       return {
+        values: values[attribute.id],
         attribute,
-        value: profile[attribute.id],
-        onChange: this.onChange,
-        extraProps: { isFocused: false, editable: false },
+        extraProps: {},
         inputId: attribute.id + "__profile__",
       };
     }
 
+    getVesselEditorProps(attribute){
+      return this.getEditorProps(attribute, this.props.vessel);
+    }
+
     render() {
-      const profile = Object.assign({}, this.props.user, this.props.vessel);
+      const styleGap = {
+        marginTop: 20,
+      }
       return (
-        <ModelEditor
-          getEditorProps={this.getEditorProps}
-          model={ ProfileModel }
-          modelValues={ profile }
-          index={1}
-          onChange={ this.onChange }
-        />
+        <View>
+          <View style={styleGap}>
+            <ModelEditor
+              getEditorProps={ this.getUserEditorProps }
+              model={ UserModel }
+              modelValues={ this.props.user }
+              index={1}
+              onChange={ this.onChangeUser }
+            />
+          </View>
+          <View style={styleGap}>
+            <ModelEditor
+              getEditorProps={ this.getEditorProps }
+              model={ VesselModel }
+              modelValues={ this.props.vessel }
+              index={1}
+              onChange={ this.onChangeVessel }
+            />
+        </View>
+      </View>
     );
   }
 }
