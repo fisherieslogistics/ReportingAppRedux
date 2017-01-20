23c0705ea105e2c8e800ded38d7c0f445178630d
fllfl <rimu@fisherieslogistics.com>
2017-01-21 06:55:04 +1300

    editable vessel and user

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -3,27 +3,27 @@ const valid = Validator.valid;
 
 const UserModel = [
   {
-    id: 'firstName', defaultValue: "", label: "First Name", valid: valid.alwaysValid,
+    id: 'firstName', defaultValue: "", label: "First Name", valid: valid.anyValue,
     display:  { type: 'single'},
   },
   {
-    id: 'lastName', defaultValue: "", label: "Last Name", valid: valid.alwaysValid,
+    id: 'lastName', defaultValue: "", label: "Last Name", valid: valid.anyValue,
     display: { type: 'single'},
   },
   {
-    id: 'permitHolderName', defaultValue: "", label: "Permit Holder Name", valid: valid.alwaysValid,
+    id: 'permitHolderName', defaultValue: "", label: "Permit Holder Name", valid: valid.anyValue,
     display: { type: 'single'},
   },
   {
-    id: 'permitHolderNumber', defaultValue: "", label: "Permit Holder Number", valid: valid.alwaysValid,
+    id: 'permitHolderNumber', defaultValue: "", label: "Permit Holder Number", valid: valid.anyValue,
     display: { type: 'single'},
   },
   {
-    id: 'email', defaultValue: "", label: "Email", valid: valid.alwaysValid,
+    id: 'email', defaultValue: "", label: "Email", valid: valid.anyValue,
     display: { type: 'single'},
   },
   {
-    id: 'contacts', defaultValue: [], valid: valid.alwaysValid,
+    id: 'contacts', defaultValue: [], valid: valid.anyValue,
   },
 ];
 
