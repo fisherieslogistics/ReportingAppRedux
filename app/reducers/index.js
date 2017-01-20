0526249882122f8dab17da908db514cd1b66eaf3
fllfl <rimu@fisherieslogistics.com>
2017-01-21 06:54:10 +1300

    send snapshot once an hour - ignore some actions

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,5 @@
 "use strict";
 import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
 import FishingEventReducer from './FishingEventReducer';
 import MeReducer from './MeReducer';
 import ViewReducer from './ViewReducer';
@@ -8,32 +7,58 @@ import TripReducer from './TripReducer';
 import FormReducer from './FormReducer';
 import Helper from '../utils/Helper';
 import EventsReducer from './EventsReducer';
-import SyncReducer from './SyncReducer';
-import APIReducer from './APIReducer';
 import MigrationReducer from './MigrationReducer';
 import HistoryReducer from './HistoryReducer';
 import ChatReducer from './ChatReducer';
-
 import moment from 'moment';
-let then = new moment();
 
 import TCPQueue from '../api/TCPQueue';
 const tcpQueue = new TCPQueue();
-
 const helper = new Helper();
-const AsyncStorage = require('AsyncStorage');
+let then = new moment();
+
+const actionsNotSending = [
+  'initAutoSuggestBarChoices',
+  'NMEAStringRecieved',
+  'toggleAutoSuggestBar',
+  'changeAutoSuggestBarText',
+  '@@redux',
+  '$$redux',
+];
+
+function createSnapshotMessage(state) {
+  const clone = Object.assign({}, state);
+  const {
+    chat,
+    fishingEvents,
+    trip,
+    forms,
+  } = clone;
+  const snapShot = JSON.stringify(helper.deflate({ chat, fishingEvents, trip, forms }));
+  const parts = snapShot.match(/[\s\S]{1,100}/g) || [];
+  const snapshotId = `${clone.trip.id}-${new moment().toISOString()}`;
+  return parts.map((part, index) => ({
+      snapshotId,
+      part,
+      index,
+  }));
+}
+
+function sendStateInParts(key, state) {
+  const snap = createSnapshotMessage(state);
+  snap.forEach((s, i) => {
+    tcpQueue.addToQueue(`{key}:${i}`, s);
+  });
+}
 
 const reducers = {
   chat: ChatReducer,
-  auth: AuthReducer,
   fishingEvents: FishingEventReducer,
   me: MeReducer,
   view: ViewReducer,
   trip: TripReducer,
   forms: FormReducer,
   uiEvents: EventsReducer,
-  sync: SyncReducer,
-  api: APIReducer,
   migrations: MigrationReducer,
   history: HistoryReducer,
 }
@@ -42,6 +67,7 @@ const MainReducer = combineReducers(reducers);
 
 const mutateState = (state, action) => {
   const newState = MainReducer(state, action);
+
   if(action.type === 'loadSavedState'){
     const loadedState = MainReducer(undefined, {type: 'init'});
     const savedState = action.savedState
@@ -57,15 +83,17 @@ const mutateState = (state, action) => {
   }
 
   helper.saveToLocalStorage(newState, action.type);
-  console.log(action.type);
-  tcpQueue.addToQueue('ACT', action);
+  if(actionsNotSending.every((str) => action.type.indexOf(str) === -1)) {
+    tcpQueue.addToQueue('ACT', action);
+  }
+
   const now = new moment();
-  console.log(now.diff(then, 'seconds'));
-  if(then.diff(now, 'seconds') > 10) {
-    console.log("Snapshot Bros");
-    const clone = Object.assign({}, newState);
-    tcpQueue.addToQueue('STE', helper.deflate({ snapshotAt: new moment().toISOString(), data: newState }));
+  if(then.diff(now, 'seconds') > 10){
+    sendStateInParts(`$SNAP-${now.toISOString()}`, newState);
     then = now;
+  }
+  if(action.type === 'endTrip'){
+    sendStateInParts('$ENDTRIP', state);
   }
   return newState;
 }
