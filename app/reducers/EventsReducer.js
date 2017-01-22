519c73ce5cc663b98586431885499ac8530a7eb8
fllfl <rimu@fisherieslogistics.com>
2017-01-23 10:30:47 +1300

    no more native gps

--- app/reducers/EventsReducer.js
+++ app/reducers/EventsReducer.js
@@ -1,12 +1,7 @@
 import EventEmitter from 'EventEmitter';
-import PositionProvider from '../providers/NativeLocation';
-
-const pos = new PositionProvider();
-//pos.startUpdatingLocation();
 
 const initialState = {
   eventEmitter: new EventEmitter(),
-  uipositionProvider: pos,
 }
 
 export default (state) => initialState
