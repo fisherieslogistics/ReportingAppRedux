7d2a1851e27e2f3e47dbb57e65b73dddb95ebf72
fllfl <rimu@fisherieslogistics.com>
2017-01-24 14:50:44 +1300

    don't repeat those grope depth, wing spread, headline height - use the ones set in trip

--- app/models/TCERFishingEventModel.js
+++ app/models/TCERFishingEventModel.js
@@ -8,20 +8,20 @@ const model = [
   {
     label: 'Groundrope Depth', id: 'groundropeDepth', valid: valid.greaterThanZero, type: 'number',
     combinedValid: { attributes: ["groundropeDepth", "bottomDepth"], func: combined.orderedGreaterThanOrEqual}, unit: 'm', optionalRender: true,
-    display: { type: 'single'}, repeating: true,
+    display: { type: 'single'},
   },
   {
     label: 'Average Speed', id: 'averageSpeed', valid: valid.greaterThanZero, type: 'float', defaultValue: 0,
     display: { type: 'single'}, unit: 'kt', optionalRender: false,
   },
   {
-    label: 'Wing Spread', id: 'wingSpread', valid: valid.greaterThanZero, type: 'number', repeating: true,
+    label: 'Wing Spread', id: 'wingSpread', valid: valid.greaterThanZero, type: 'number',
     display: { type: 'combined', siblings: ['headlineHeight']}, unit: 'm', order: 1, optionalRender: true,
   },
   {
     label: 'Headline Height', id: 'headlineHeight', valid: valid.greaterThanZero,
-    type: 'float', unit: 'm', defaultValue: 0.8, optionalRender: true,
-    display: { type: 'child'}, repeating: true,
+    type: 'float', unit: 'm', defaultValue: 0, optionalRender: true,
+    display: { type: 'child'},
   },
   {label: 'Non Fish Protected Species',  type: 'bool', id: 'nonFishProtected', valid: valid.alwaysValid, defaultValue: false,
     display: { type: 'single' }, optionalRender: true,
